55f8c151edda7af321a4fce4402257388ad0bcc6
FyiurAmron <spamove@gmail.com>
2016-05-09 18:48:55 +0200

    basic Swing UI implemented

--- src/main/java/vax/celloscope/Binarize.java
+++ src/main/java/vax/celloscope/Binarize.java
@@ -1,9 +1,11 @@
 package vax.celloscope;
 
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+import java.awt.image.WritableRaster;
 import javax.swing.JFrame;
 
 import org.opencv.core.Mat;
-import org.opencv.imgproc.Imgproc;
 import static org.opencv.imgproc.Imgproc.*;
 import static org.opencv.highgui.Highgui.*;
 
@@ -12,36 +14,48 @@ import static org.opencv.highgui.Highgui.*;
  @author toor
  */
 public class Binarize {
-    final int max_BINARY_value = 255;
+    final static public int MAX_8BIT_VALUE = 255;
 
-    Mat src, src_gray = new Mat(), dst = new Mat();
-    String window_name = "Threshold Demo";
+    private Mat src;
+    private final Mat srcGray = new Mat(), dst = new Mat();
 
-    void demo ( String srcName ) {
-        /// Load an image
+    public Mat loadImage ( String srcName ) {
+        // Load an image
         src = imread( srcName, 1 );
-        if ( src == null ) {
-            return;
-        }
 
-        JFrame jf = new JFrame( window_name );
-        jf.setDefaultCloseOperation( JFrame.DISPOSE_ON_CLOSE );
-        jf.pack();
-        jf.setVisible( true );
-
-        /// Convert the image to Gray
-        cvtColor( src, src_gray, COLOR_BGR2GRAY );
-
+        // Convert the image to Gray
+        cvtColor( src, srcGray, COLOR_BGR2GRAY );
+        return srcGray;
     }
 
-    void thresholdTest () {
-        if ( src == null ) {
-            return;
-        }
-        //threshold( src_gray, dst, threshold_value, max_BINARY_value, threshold_type );
-        threshold( src_gray, dst, 0, max_BINARY_value, THRESH_OTSU );
+    public Mat thresholdTest () {
+        //threshold( src_gray, dst, threshold_value, MAX_8BIT_VALUE, threshold_type );
+        threshold( srcGray, dst, 0, MAX_8BIT_VALUE, THRESH_OTSU );
 
         //imshow( window_name, dst );
         imwrite( "output.png", dst );
+        return dst;
+    }
+
+    public static BufferedImage createAwtImage ( Mat mat ) {
+        int type;
+        switch ( mat.channels() ) {
+            case 1:
+                type = BufferedImage.TYPE_BYTE_GRAY;
+                break;
+            case 3:
+                type = BufferedImage.TYPE_3BYTE_BGR;
+                break;
+            default:
+                return null;
+        }
+
+        BufferedImage image = new BufferedImage( mat.width(), mat.height(), type );
+        WritableRaster raster = image.getRaster();
+        DataBufferByte dataBuffer = (DataBufferByte) raster.getDataBuffer();
+        byte[] data = dataBuffer.getData();
+        mat.get( 0, 0, data );
+
+        return image;
     }
 }
--- src/main/java/vax/celloscope/Main.java
+++ src/main/java/vax/celloscope/Main.java
@@ -1,16 +1,30 @@
 package vax.celloscope;
 
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
 /**
 
  @author toor
  */
 public class Main {
+    final static private String WINDOW_NAME = "Threshold Demo";
+
     public static void main ( String[] args ) {
         nu.pattern.OpenCV.loadShared();
+
+        JFrame jf = new JFrame( WINDOW_NAME );
+        jf.setDefaultCloseOperation( JFrame.DISPOSE_ON_CLOSE );
+
         //System.out.println( "test" );
         Binarize b = new Binarize();
-        //b.demo( "test.png" );
-        b.demo( "E:\\Develop\\Java\\_projects\\celloscope\\src\\main\\resources\\test.png" );
-        b.thresholdTest();
+        //b.loadImage( "test.png" );
+        b.loadImage( "E:\\Develop\\Java\\_projects\\celloscope\\src\\main\\resources\\test.png" );
+        ImageIcon image = new ImageIcon( Binarize.createAwtImage( b.thresholdTest() ) );
+        jf.add( new JLabel( image ) );
+
+        jf.pack();
+        jf.setVisible( true );
     }
 }
